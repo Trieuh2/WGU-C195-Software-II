a049f281e7faca2f936426aa26844d941bff55eb
Henry Trieu <henrytrieu88@gmail.com>
2021-09-21 22:45:21 -0700

    Initial Commit

---
+++ src/Scheduler/View_Controller/MainController.java
@@ -0,0 +1,17 @@
+package Scheduler.View_Controller;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.application.Application;
+import javafx.fxml.Initializable;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+
+    }
+}
