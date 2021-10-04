1dae3d47f75b8d4f12e8ba5fe5406726200c944a
Henry Trieu <henrytrieu88@gmail.com>
2021-10-03 17:16:25 -0700

    Update

---
+++ src/Scheduler/View_Controller/AddCustomerController.java
@@ -0,0 +1,16 @@
+package Scheduler.View_Controller;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class AddCustomerController extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
--- src/Scheduler/View_Controller/MainController.java
+++ src/Scheduler/View_Controller/MainController.java
@@ -3,15 +3,50 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.application.Application;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainController implements Initializable {
 
+    // Primary AnchorPane on the Main Controller
+    @FXML private AnchorPane mainAnchorPane;
+
+    // Buttons
+    @FXML private Button addCustomerButton;
+    @FXML private Button addAppointmentButton;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
 
+    }
+
+    // Closes the main screen and switches to the controller where the user is prompted to fill out information to
+    // add a new user.
+    private void switchToAddCustomerController() throws IOException {
+        // Load the FXML file.
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/Scheduler/View_Controller/AddCustomerController.fxml"));
+        AddCustomerController controller = new AddCustomerController();
+        loader.setController(controller);
+        Parent root = loader.load();
+
+        // Close the current window and build the MainController scene to display the appointment calendar
+        closeCurrentWindow();
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    // Closes the current window
+    private void closeCurrentWindow() {
+        Stage currentStage = (Stage)mainAnchorPane.getScene().getWindow();
+        currentStage.close();
     }
 }
